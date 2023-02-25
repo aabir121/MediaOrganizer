3f4d081fcc77a6831797e0d006b189d228cdc98b
Aabir Hassan <aabir121@gmail.com>
2023-02-25 16:49:28 +0600

    Added source directory chooser button.

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.directoryChooserBtn = new System.Windows.Forms.Button();
+            this.sourceDirTxtBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // directoryChooserBtn
+            // 
+            this.directoryChooserBtn.Location = new System.Drawing.Point(1160, 53);
+            this.directoryChooserBtn.Name = "directoryChooserBtn";
+            this.directoryChooserBtn.Size = new System.Drawing.Size(453, 58);
+            this.directoryChooserBtn.TabIndex = 1;
+            this.directoryChooserBtn.Text = "Choose Directory";
+            this.directoryChooserBtn.UseVisualStyleBackColor = true;
+            this.directoryChooserBtn.Click += new System.EventHandler(this.directoryChooserBtn_Click);
+            // 
+            // sourceDirTxtBox
+            // 
+            this.sourceDirTxtBox.CausesValidation = false;
+            this.sourceDirTxtBox.Location = new System.Drawing.Point(40, 59);
+            this.sourceDirTxtBox.Name = "sourceDirTxtBox";
+            this.sourceDirTxtBox.PlaceholderText = "Enter folder path to organize images and videos";
+            this.sourceDirTxtBox.ReadOnly = true;
+            this.sourceDirTxtBox.Size = new System.Drawing.Size(1095, 47);
+            this.sourceDirTxtBox.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1665, 1095);
+            this.Controls.Add(this.sourceDirTxtBox);
+            this.Controls.Add(this.directoryChooserBtn);
+            this.Name = "MainForm";
+            this.Text = "Media Organizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button directoryChooserBtn;
+        private TextBox sourceDirTxtBox;
     }
 }
--- MainForm.cs
+++ MainForm.cs
@@ -6,5 +6,17 @@ namespace MediaOrganizer
         {
             InitializeComponent();
         }
+
+        private void directoryChooserBtn_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = "Select a folder to organize media files";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    sourceDirTxtBox.Text = dialog.SelectedPath;
+                }
+            }
+        }
     }
 }
